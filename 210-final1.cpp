51bc858f5a94103244f198c91cd8baa21a502d0c
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-12-10 16:45:42 -0800

    reading file

--- 210-final1.cpp
+++ 210-final1.cpp
@@ -1,10 +1,22 @@
 // COMSC210 | Final Exam 1 | Majd Bohsali
 // IDE used: Visaul Studio Code
-
 #include <iostream>
-
+#include <map>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int main() { 
+    map<string, int> airportLog; 
+
+    ifstream inputFile("210-final-1-FA25"); 
+
+    if(inputFile.is_open()) { 
+
+        inputFile.close();
+    } else { 
+        cout << "File could not open"
+    }
+
     return 0; 
 }
