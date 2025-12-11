11c673d820327339e97d199b3288b0444bda9633
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-12-10 16:52:19 -0800

    Milestone 1

--- 210-final1.cpp
+++ 210-final1.cpp
@@ -4,18 +4,29 @@
 #include <map>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
 
 int main() { 
     map<string, int> airportLog; 
 
-    ifstream inputFile("210-final-1-FA25"); 
+    ifstream inputFile("210-final-1-FA25.txt"); 
+
+    string airportOrigin, airportDest; 
 
     if(inputFile.is_open()) { 
-
+        while(inputFile >> airportOrigin >> airportDest) { 
+            airportLog[airportOrigin] += 1;
+            airportLog[airportDest] += 1; 
+        }
         inputFile.close();
     } else { 
-        cout << "File could not open"
+        cout << "File could not open";
+    }
+
+    for(auto it = airportLog.begin(); it != airportLog.end(); it++) { 
+        int airportCount = it->second; 
+        cout << it->first << ": " << airportCount << endl; 
     }
 
     return 0; 
